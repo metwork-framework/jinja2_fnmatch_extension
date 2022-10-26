5f1635c6ebadfea9923e8fc95a6bf4252123ea85
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-10-26 15:33:02 +0000

    build: release 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author="Fabien MARTY",
     author_email="fabien.marty@gmail.com",
     name="jinja2_fnmatch_extension",
-    version="1.0.0",
+    version="1.0.1",
     license="BSD",
     packages=find_packages(),
     install_requires=install_requires,
